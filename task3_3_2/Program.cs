a17f3146fd284a9281a2398a5f2033489176cf6b
Yulia Miller <miller82.98@mail.ru>
2022-12-05 12:27:08 -0800

    Добавьте файлы проекта.

---
+++ task3_3_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MyProgramm
+{
+    public static void Main(string[] args)
+    {
+        string MyName = "Yliua";
+        int Age = 27;
+        bool Pets = true;
+        double MySize = 36;
+
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("I am " + Age + " years old");
+        Console.WriteLine("Do you have pats " + Pets);
+        Console.WriteLine("My size " + MySize);
+    }
+}
